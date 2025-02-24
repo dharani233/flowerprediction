8267958ae476c3e2756434a9e9fe49c115f7f811
dharani233 <dharanidjune23@gmail.com>
2025-02-24 15:51:41 +0530

    Add files via upload

---
+++ mini   project/model.py
@@ -0,0 +1,34 @@
+#import all the dependencies
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+#load the csv file
+df = pd.read_csv('iris.csv')
+print(df.head())
+print('----------------------------------------------------------------------------')
+print(df.tail())
+
+#Select independent and dependent variable
+x=df[["Sepal_Length","Sepal_Width","Petal_Length","Petal_Width"]]
+y=df["Class"]
+
+#Split the dataset into train and test
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=50)
+
+
+# Feature Scaling
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+#model selection
+classifier = RandomForestClassifier()
+
+# Fit the model
+classifier.fit(x_train, y_train)
+
+# make pickle file of our model
+pickle.dump(classifier, open("model.pkl", "wb"))
